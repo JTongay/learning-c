31cacfe536d05632d05b32d4c33682e593be1589
Joseph Tongay <joseph.tongay@gmail.com>
2025-10-17 05:42:25 -0700

    temps

--- lesson-1/2-temps.c
+++ lesson-1/2-temps.c
@@ -4,8 +4,23 @@
  * print Farenheit-Celcius Table for fahr = 0, 20, ..., 300
  */
 
+void celciusToFarenheit(int lower, int upper, int step) {
+  float fahr, celsius;
+
+  celsius = lower;
+  printf("Celcius Farenheit\n");
+
+  while (celsius <= upper) {
+    fahr = celsius * (9.0 / 5.0) + 32.0;
+    printf("%3.0f %6.1f\n", celsius, fahr);
+
+    celsius = celsius + step;
+  }
+}
+
 int main() {
-  int fahr, celsius;
+  float fahr, celsius; /* Use Floating point arithmetic instead of integer for
+                          more accurate results */
   int lower, upper, step;
 
   lower = 0;   /* lower limit of temperature table */
@@ -13,11 +28,16 @@ int main() {
   step = 20;   /* step size */
 
   fahr = lower;
+  printf("Farenheit Celcius\n");
   while (fahr <= upper) {
-    celsius = 5 * (fahr - 32) / 9;
-    printf("%3d %6d\n", fahr, celsius);
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.0f %6.1f\n", fahr,
+           celsius); /* Notice the 6.1 in the print here, it says to print 1
+                        digit after the decimal point */
     fahr = fahr + step;
   }
+
+  celciusToFarenheit(0, 300, 20);
 
   return 1;
 }
