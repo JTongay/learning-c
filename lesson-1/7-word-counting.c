35f6ea211f470c14c97f94bb7a0331b60a12d5db
Joseph Tongay <joseph.tongay@gmail.com>
2025-11-17 14:09:53 -0800

    We're in there

---
+++ lesson-1/7-word-counting.c
@@ -0,0 +1,36 @@
+/**
+ * Barebones version of counting words. We're assuming that a word is any
+ * sequence of characters that does not contain a blank, tab, or newline.
+ */
+
+#include <stdio.h>
+
+#define IN 1  /* Inside a word */
+#define OUT 0 /* Outside a word */
+
+// Count lines, words, and characters in input
+
+int main() {
+  int c, nl, nw, nc, state;
+
+  state = OUT;
+  nl = nw = nc = 0;
+  while ((c = getchar()) != EOF) {
+    ++nc;
+
+    if (c == '\n') {
+      ++nl;
+    }
+
+    if (c == ' ' || c == '\n' || c == '\t') {
+      state = OUT;
+    }
+
+    else if (state == OUT) {
+      state = IN;
+      ++nw;
+    }
+  }
+
+  printf("%d %d %d\n", nl, nw, nc);
+}
